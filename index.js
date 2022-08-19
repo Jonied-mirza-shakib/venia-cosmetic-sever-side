d627012293f5a0cab3d568e88e09c5abaabb4e1a
Jonied mirza shakib <MDSHAKIB01088@gmail.com>
2022-08-19 15:06:24 +0600

    updated product successfully

--- index.js
+++ index.js
@@ -21,59 +21,92 @@ async function run() {
         const blogCollection = client.db('cosmetic').collection('blog');
         const commentsCollection = client.db('cosmetic').collection('comments');
         const orderCollection = client.db('cosmetic').collection('order');
+        const usersCollection = client.db('cosmetic').collection('users');
 
         app.post('/products', async (req, res) => {
             const query = req.body;
             const result = await productCollection.insertOne(query);
             res.send(result)
-          })
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+
+
+
+
         app.get('/products', async (req, res) => {
             const query = {};
             const result = await productCollection.find(query).toArray();
             res.send(result)
-          })
+        })
         app.get('/products/:id', async (req, res) => {
-            const id=req.params.id;
-            const query = {_id: ObjectId(id)};
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
             const result = await productCollection.findOne(query)
             res.send(result)
-          })
-        app.post('/blog',async(req,res)=>{
-            const query=req.body;
-            const result=await blogCollection.insertOne(query);
-            res.send(result)
-        })
-        app.get('/blog',async(req,res)=>{
-            const query={};
-            const result=await blogCollection.find(query).toArray();
-            res.send(result)
-        })
-        app.get('/blog/:id',async(req,res)=>{
-           const id=req.params.id;
-           const query={_id:ObjectId(id)};
-           const result=await blogCollection.findOne(query);
-           res.send(result)
         })
 
-        app.get('/comments',async(req,res)=>{
-            const query={};
-            const result=await commentsCollection.find(query).toArray();
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateProduct = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: updateProduct.name,
+                    price: updateProduct.price,
+                    img: updateProduct.img
+                },
+            };
+            const result = await productCollection.updateOne(filter, updateDoc, options);
             res.send(result)
         })
-        app.post('/comments',async(req,res)=>{
-            const query=req.body;
-            const result= await commentsCollection.insertOne(query);
+        app.post('/blog', async (req, res) => {
+            const query = req.body;
+            const result = await blogCollection.insertOne(query);
             res.send(result)
         })
-        app.get('/order',async(req,res)=>{
-            const email=req.query.email;
-            const query={email:email};
-            const result=await orderCollection.find(query).toArray();
+        app.get('/blog', async (req, res) => {
+            const query = {};
+            const result = await blogCollection.find(query).toArray();
             res.send(result)
         })
-        app.post('/order',async(req,res)=>{
-            const query=req.body;
-            const result= await orderCollection.insertOne(query);
+        app.get('/blog/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await blogCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.get('/comments', async (req, res) => {
+            const query = {};
+            const result = await commentsCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.post('/comments', async (req, res) => {
+            const query = req.body;
+            const result = await commentsCollection.insertOne(query);
+            res.send(result)
+        })
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await orderCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.post('/order', async (req, res) => {
+            const query = req.body;
+            const result = await orderCollection.insertOne(query);
             res.send(result)
         })
 
